a01f567e6dccd4cbb8bd552c3594b60d60552573
jonathan <jonathanleek@gmail.com>
2020-04-27 22:55:13 -0500

    Initial Cloning of REDB-Infrastructure. Removed REDB-Platform module. Backend configured.

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
-#-------------------------------------------------------
-#Create Airflow Cluster
-#-------------------------------------------------------
-module "redb-platform" {
-  source = "git::https://github.com/stlrda/REDB-Platform"
-  cluster_name = var.cluster_name
-  tags = var.tags
-  aws_region = var.aws_region
-  aws_profile = var.aws_profile
-  ec2_keypair_name = var.ec2_keypair_name
-  public_key_path = var.public_key_path
-  private_key_path = var.public_key_path
-  admin_email = var.admin_email
-  admin_username = var.admin_username
-  admin_password = var.admin_password
-  fernet_key = var.fernet_key
-  time_zone = var.time_zone
-  load_examples = var.load_examples
-  webserver_instance_type = var.webserver_instance_type
-  scheduler_instance_type = var.scheduler_instance_type
-  worker_instance_type = var.worker_instance_type
-  number_of_workers  = var.number_of_workers
-  db_instance_type = var.db_instance_type
-  db_dbname = var.db_dbname
-  db_username = var.db_username
-  db_password = var.db_password
-  dag_git_repository_url=var.dag_git_repository_url
-  dag_git_repository_directory=var.dag_git_repository_directory
-  dag_git_repository_branch=var.dag_git_repository_branch
-}
 
 #-------------------------------------------------------------
 #Create S3 bucket as ETL processes workspace
@@ -45,7 +15,7 @@ resource "aws_s3_bucket" "airflow_workbucket" {
 resource "aws_security_group" "redb_db_security_group" {
   name = "redb_db_security_group"
   description = "Security group for target database"
-  vpc_id = module.redb-platform.cluster_vpc_id
+//  vpc_id = module.redb-platform.cluster_vpc_id
   tags = var.tags
 
     ingress {
@@ -109,6 +79,6 @@ resource "aws_db_instance" "airflow_database" {
   skip_final_snapshot     = true
   vpc_security_group_ids  = ["${aws_security_group.redb_db_security_group.id}"]
   port                    = 5432
-  db_subnet_group_name    = module.redb-platform.db_subnet_group
+//  db_subnet_group_name    = module.redb-platform.db_subnet_group
   allocated_storage       = 20
 }
